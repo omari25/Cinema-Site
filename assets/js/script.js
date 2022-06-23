22be52f569a90c7ce5b69f3d05a6e6fc9c0977cc
Omar Mahat <omar.mahat@student.moringaschool.com>
2022-06-23 21:41:02 +0300

    manipulating the DOM by displaying the data in the DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,9 +19,29 @@ form.addEventListener('submit', (e) => {
     }
 })
 
-
 async function getMovies(url) {
     const res = await fetch(url)
     const data = await res.json()
     showMovies(data.results)
+}
+
+function showMovies(movies) {
+    main.innerHTML = ''
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average, overview } = movie
+        const movieObjects = document.createElement('div')
+        movieObjects.classList.add('movie')
+        movieObjects.innerHTML = `
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${movieRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+            </div>
+        `
+        main.appendChild(movieObjects)
+    })
 }
