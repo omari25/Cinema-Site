d6079702e468b6358a61a6552e494211cabeb25e
Omar Mahat <omar.mahat@student.moringaschool.com>
2022-06-23 21:32:23 +0300

    grab the api links

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3e54d07be171d1072d3804c8c27094dc&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3e54d07be171d1072d3804c8c27094dc&query="'
